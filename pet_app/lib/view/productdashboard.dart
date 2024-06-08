7e770291f61e980c60842f04e7a584a7c10fe7d0
monica zanje <fluttermonika@gmail.com>
2024-06-09 00:34:27 +0530

    pet app

--- pet_app/lib/view/productdashboard.dart
+++ pet_app/lib/view/productdashboard.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class DashBoardScreen extends StatefulWidget {
   const DashBoardScreen({super.key});
@@ -9,6 +12,297 @@ class DashBoardScreen extends StatefulWidget {
 class _DashBoardScreenState extends State {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 252, 251, 251),
+      body: Container(
+        margin: const EdgeInsets.only(left: 24, right: 24, top: 60),
+        child: Column(
+          children: [
+            SizedBox(
+              child: Row(
+                children: [
+                  Container(
+                    height: 56,
+                    width: 56,
+                    decoration: const BoxDecoration(
+                      shape: BoxShape.circle,
+                      image: DecorationImage(
+                        image: AssetImage("assets/image.png"),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.all(8),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Hello, Sarah",
+                          style: GoogleFonts.poppins(
+                            fontSize: 14,
+                            fontWeight: FontWeight.w500,
+                            color: const Color.fromRGBO(0, 0, 0, 1),
+                          ),
+                        ),
+                        Text(
+                          "Good Morning!",
+                          style: GoogleFonts.poppins(
+                            fontSize: 14,
+                            fontWeight: FontWeight.w400,
+                            color: const Color.fromRGBO(0, 0, 0, 1),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const Spacer(
+                    flex: 1,
+                  ),
+                  GestureDetector(
+                    onTap: () {},
+                    child: const Icon(
+                      Icons.notifications_outlined,
+                      size: 24,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 23,
+            ),
+            Container(
+              padding: const EdgeInsets.only(
+                  left: 16, top: 11, bottom: 11, right: 16),
+              decoration: BoxDecoration(
+                  border: Border.all(
+                    color: const Color.fromRGBO(245, 146, 69, 1),
+                    style: BorderStyle.solid,
+                  ),
+                  borderRadius: const BorderRadius.all(Radius.circular(10))),
+              child: Row(
+                children: [
+                  SizedBox(
+                    child: Text(
+                      "search",
+                      style: GoogleFonts.poppins(
+                        fontSize: 12,
+                        fontWeight: FontWeight.w400,
+                        color: const Color.fromRGBO(0, 0, 0, 1),
+                      ),
+                    ),
+                  ),
+                  const Spacer(
+                    flex: 1,
+                  ),
+                  const Icon(
+                    Icons.search,
+                    size: 24,
+                    color: Color.fromRGBO(245, 146, 69, 1),
+                  )
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Container(
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.all(Radius.circular(20)),
+                boxShadow: [
+                  BoxShadow(
+                    blurRadius: 16,
+                    spreadRadius: 5,
+                    offset: Offset(0, 8),
+                    color: Color.fromRGBO(22, 34, 51, 0.08),
+                  ),
+                ],
+              ),
+              child: Row(
+                children: [
+                  Container(
+                    padding:
+                        const EdgeInsets.only(top: 16, bottom: 16, left: 10),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "In Love With Pets?",
+                          style: GoogleFonts.poppins(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w600,
+                            color: const Color.fromRGBO(0, 0, 0, 1),
+                          ),
+                        ),
+                        Text(
+                          "Get all what you need for them",
+                          style: GoogleFonts.poppins(
+                            fontSize: 14,
+                            fontWeight: FontWeight.w600,
+                            color: const Color.fromRGBO(245, 146, 69, 1),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    padding: const EdgeInsets.only(right: 5, top: 16),
+                    child:
+                        Image.asset('assets/image (20).png', fit: BoxFit.cover),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Container(
+              margin: const EdgeInsets.all(10),
+              child: Row(
+                children: [
+                  Text(
+                    "Category",
+                    style: GoogleFonts.inter(
+                      fontSize: 14,
+                      fontWeight: FontWeight.w500,
+                      color: const Color.fromRGBO(0, 0, 0, 1),
+                    ),
+                  ),
+                  const Spacer(
+                    flex: 1,
+                  ),
+                  GestureDetector(
+                    onTap: () {},
+                    child: Text(
+                      "See All",
+                      style: GoogleFonts.inter(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w400,
+                        color: const Color.fromARGB(194, 62, 76, 43),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            SizedBox(
+              child: Row(
+                children: [
+                  GestureDetector(
+                    onTap: () {},
+                    child: Column(
+                      children: [
+                        Container(
+                          decoration:
+                              const BoxDecoration(shape: BoxShape.circle),
+                          child: Image.asset('assets/image (1).png'),
+                        ),
+                        Text(
+                          "Veterinary",
+                          style: GoogleFonts.inter(
+                            fontSize: 12,
+                            fontWeight: FontWeight.w500,
+                            color: const Color.fromRGBO(0, 0, 0, 1),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const Spacer(
+                    flex: 1,
+                  ),
+                  GestureDetector(
+                    onTap: () {},
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(
+                          decoration:
+                              const BoxDecoration(shape: BoxShape.circle),
+                          child: Image.asset('assets/image (2).png'),
+                        ),
+                        Text(
+                          "Grooming",
+                          style: GoogleFonts.inter(
+                            fontSize: 12,
+                            fontWeight: FontWeight.w500,
+                            color: const Color.fromRGBO(0, 0, 0, 1),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const Spacer(
+                    flex: 1,
+                  ),
+                  GestureDetector(
+                    onTap: () {},
+                    child: Column(
+                      children: [
+                        Container(
+                          decoration:
+                              const BoxDecoration(shape: BoxShape.circle),
+                          child: Image.asset('assets/image (3).png'),
+                        ),
+                        Text(
+                          "Pet Store",
+                          style: GoogleFonts.inter(
+                            fontSize: 12,
+                            fontWeight: FontWeight.w500,
+                            color: const Color.fromRGBO(0, 0, 0, 1),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const Spacer(
+                    flex: 1,
+                  ),
+                  GestureDetector(
+                    onTap: () {},
+                    child: Column(
+                      children: [
+                        Container(
+                          decoration:
+                              const BoxDecoration(shape: BoxShape.circle),
+                          child: Image.asset('assets/image (4).png'),
+                        ),
+                        Text(
+                          "Training",
+                          style: GoogleFonts.inter(
+                            fontSize: 12,
+                            fontWeight: FontWeight.w500,
+                            color: const Color.fromRGBO(0, 0, 0, 1),
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Container(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                "Event",
+                style: GoogleFonts.inter(
+                  fontSize: 14,
+                  fontWeight: FontWeight.w600,
+                  color: const Color.fromRGBO(0, 0, 0, 1),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
