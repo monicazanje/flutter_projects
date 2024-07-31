9319f8f4bbf1bbd90118802b68f42790c27e8576
monica zanje <fluttermonika@gmail.com>
2024-07-31 23:47:08 +0530

    fitness  app

--- fitness_app/lib/view/activity_screen.dart
+++ fitness_app/lib/view/activity_screen.dart
@@ -1,6 +1,9 @@
 import 'package:fitness_app/view/navigation.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:percent_indicator/circular_percent_indicator.dart';
 
 class ActivityScreen extends StatefulWidget {
   const ActivityScreen({super.key});
@@ -43,65 +46,342 @@ class _ActivityState extends State<ActivityScreen> {
           Expanded(
             child: SizedBox(
               child: ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  itemCount: 30,
-                  itemBuilder: (context, index) {
-                    bool isSelected = selectedIndex == index;
-                    return GestureDetector(
-                      onTap: () {
-                        setState(() {
-                          selectedIndex = index;
-                          if (isSelected == false) {
-                            isSelected = true;
-                          } else {
-                            isSelected = false;
-                          }
-                        });
-                      },
-                      child: Container(
-                        margin: const EdgeInsets.all(10),
-                        padding: const EdgeInsets.only(
-                            left: 20, right: 20, top: 10, bottom: 10),
-                        height: 50,
-                        decoration: BoxDecoration(
-                          borderRadius: const BorderRadius.all(
-                            Radius.circular(20),
-                          ),
-                          color: isselected
-                              ? const Color.fromRGBO(0, 0, 0, 1)
-                              : const Color.fromRGBO(187, 242, 70, 1),
+                scrollDirection: Axis.horizontal,
+                itemCount: 30,
+                itemBuilder: (context, index) {
+                  bool isSelected = selectedIndex == index;
+                  return GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        selectedIndex = index;
+                        if (isSelected == false) {
+                          isSelected = true;
+                        } else {
+                          isSelected = false;
+                        }
+                      });
+                    },
+                    child: Container(
+                      margin: const EdgeInsets.all(10),
+                      padding: const EdgeInsets.only(
+                          left: 20, right: 20, top: 5, bottom: 5),
+                      // height: 50,
+                      decoration: BoxDecoration(
+                        borderRadius: const BorderRadius.all(
+                          Radius.circular(20),
                         ),
-                        child: Column(
+                        color: isselected
+                            ? const Color.fromRGBO(0, 0, 0, 1)
+                            : const Color.fromRGBO(187, 242, 70, 1),
+                      ),
+                      child: Column(
+                        children: [
+                          Text(
+                            "S",
+                            style: GoogleFonts.lato(
+                              fontSize: 18,
+                              fontWeight: FontWeight.w700,
+                              color: isselected
+                                  ? const Color.fromARGB(255, 252, 252, 252)
+                                  : const Color.fromRGBO(0, 0, 0, 1),
+                            ),
+                          ),
+                          Text(
+                            "${index + 1}",
+                            style: GoogleFonts.lato(
+                              fontSize: 18,
+                              fontWeight: FontWeight.w700,
+                              color: isselected
+                                  ? const Color.fromARGB(255, 252, 252, 252)
+                                  : const Color.fromRGBO(0, 0, 0, 1),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ),
+          Container(
+            margin:
+                const EdgeInsets.only(left: 20, right: 20, top: 20, bottom: 10),
+            child: Text(
+              "Today Report",
+              style: GoogleFonts.lato(
+                fontSize: 18,
+                fontWeight: FontWeight.w700,
+                color: const Color.fromRGBO(31, 32, 41, 1),
+              ),
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.only(left: 20, right: 20),
+            child: Row(
+              children: [
+                Column(
+                  children: [
+                    Container(
+                      height: 70,
+                      width: 115,
+                      margin: const EdgeInsets.all(10),
+                      padding: const EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          style: BorderStyle.solid,
+                        ),
+                        borderRadius: const BorderRadius.all(
+                          Radius.circular(20),
+                        ),
+                      ),
+                      child: Column(
+                        children: [
+                          Text(
+                            "Active calories",
+                            style: GoogleFonts.lato(
+                              fontSize: 13,
+                              fontWeight: FontWeight.w500,
+                              color: const Color.fromRGBO(25, 33, 38, 0.5),
+                            ),
+                          ),
+                          Text(
+                            "645 Cal",
+                            style: GoogleFonts.lato(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w600,
+                              color: const Color.fromRGBO(31, 32, 41, 1),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      margin: const EdgeInsets.only(bottom: 10),
+                      padding: const EdgeInsets.only(
+                          left: 20, right: 20, top: 10, bottom: 10),
+                      decoration: const BoxDecoration(
+                        color: Color.fromRGBO(234, 236, 255, 1),
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(20),
+                        ),
+                      ),
+                      child: Column(
+                        children: [
+                          Text(
+                            "Training time",
+                            style: GoogleFonts.lato(
+                              fontSize: 13,
+                              fontWeight: FontWeight.w500,
+                              color: const Color.fromRGBO(25, 33, 38, 1),
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          CircularPercentIndicator(
+                            radius: 40.0,
+                            lineWidth: 15.0,
+                            percent: 0.8,
+                            reverse: true,
+                            center: Text(
+                              "80%",
+                              style: GoogleFonts.lato(
+                                fontSize: 13,
+                                fontWeight: FontWeight.w800,
+                                color: const Color.fromARGB(255, 0, 0, 0),
+                              ),
+                            ),
+                            backgroundColor: Colors.white,
+                            progressColor:
+                                const Color.fromRGBO(164, 138, 237, 1),
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                Container(
+                  margin: const EdgeInsets.only(left: 10),
+                  padding: const EdgeInsets.only(
+                      left: 10, right: 10, top: 15, bottom: 15),
+                  decoration: const BoxDecoration(
+                    color: Colors.black,
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(20),
+                    ),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: [
+                          Container(
+                            margin: const EdgeInsets.only(left: 20),
+                            padding: const EdgeInsets.all(5),
+                            decoration: const BoxDecoration(
+                              color: Colors.white,
+                            ),
+                            child: Image.asset(
+                              "assets/icon (16).png",
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                          const SizedBox(
+                            width: 10,
+                          ),
+                          Text(
+                            "Cycling",
+                            style: GoogleFonts.lato(
+                              fontSize: 18,
+                              fontWeight: FontWeight.w800,
+                              color: const Color.fromRGBO(255, 255, 255, 1),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Container(
+                        padding: const EdgeInsets.all(5),
+                        margin: const EdgeInsets.all(10),
+                        width: 180,
+                        height: 146,
+                        decoration: const BoxDecoration(
+                          color: Colors.white,
+                        ),
+                        child: Image.asset(
+                          "assets/Map.png",
+                          fit: BoxFit.fill,
+                          scale: 1,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.only(left: 10, right: 10),
+            child: Row(
+              children: [
+                Container(
+                  width: 200,
+                  margin: const EdgeInsets.only(left: 10),
+                  padding: const EdgeInsets.only(top: 15, bottom: 15),
+                  decoration: const BoxDecoration(
+                    color: Color.fromRGBO(255, 235, 235, 1),
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(20),
+                    ),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: [
+                          Container(
+                            margin: const EdgeInsets.only(left: 20),
+                            padding: const EdgeInsets.all(5),
+                            decoration: const BoxDecoration(
+                              color: Color.fromRGBO(249, 185, 185, 1),
+                            ),
+                            child: Image.asset(
+                              "assets/icon (15).png",
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                          const SizedBox(
+                            width: 10,
+                          ),
+                          Text(
+                            "Hearth Rate",
+                            style: GoogleFonts.lato(
+                              fontSize: 18,
+                              fontWeight: FontWeight.w800,
+                              color: const Color.fromRGBO(0, 0, 0, 1),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Container(
+                        padding: const EdgeInsets.all(10),
+                        margin: const EdgeInsets.all(10),
+                        // width: 148,
+                        height: 90,
+                        decoration: const BoxDecoration(
+                          color: Colors.white,
+                        ),
+                        child: Stack(
                           children: [
-                            Text(
-                              "S",
-                              style: GoogleFonts.lato(
-                                fontSize: 18,
-                                fontWeight: FontWeight.w700,
-                                color: isselected
-                                    ? const Color.fromARGB(255, 252, 252, 252)
-                                    : const Color.fromRGBO(0, 0, 0, 1),
+                            SizedBox(
+                              width: 148,
+                              height: 51,
+                              child: Image.asset(
+                                "assets/Heartrate.png",
+                                fit: BoxFit.fill,
+                                scale: 1,
                               ),
                             ),
-                            Text(
-                              "${index + 1}",
-                              style: GoogleFonts.lato(
-                                fontSize: 18,
-                                fontWeight: FontWeight.w700,
-                                color: isselected
-                                    ? const Color.fromARGB(255, 252, 252, 252)
-                                    : const Color.fromRGBO(0, 0, 0, 1),
+                            Positioned(
+                              right: 0,
+                              bottom: 0,
+                              child: Text(
+                                "79 Bpm",
+                                style: GoogleFonts.lato(
+                                  fontSize: 12,
+                                  fontWeight: FontWeight.w600,
+                                  color: const Color.fromRGBO(0, 0, 0, 1),
+                                ),
                               ),
-                            ),
+                            )
                           ],
                         ),
                       ),
-                    );
-                  }),
+                    ],
+                  ),
+                ),
+                Column(
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.all(10),
+                      margin: const EdgeInsets.only(left: 10),
+                      decoration: const BoxDecoration(
+                        color: Color.fromRGBO(255, 232, 198, 1),
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(20),
+                        ),
+                      ),
+                      child: Row(
+                        children: [
+                          Container(
+                            margin: const EdgeInsets.only(left: 20),
+                            padding: const EdgeInsets.all(5),
+                            decoration: const BoxDecoration(
+                              color: Color.fromRGBO(248, 211, 157, 1),
+                            ),
+                            child: Image.asset(
+                              "assets/ic_steps.png",
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                          const SizedBox(
+                            width: 10,
+                          ),
+                          Text(
+                            "Steps",
+                            style: GoogleFonts.lato(
+                              fontSize: 18,
+                              fontWeight: FontWeight.w800,
+                              color: const Color.fromRGBO(0, 0, 0, 1),
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                )
+              ],
             ),
-          ),
-          const Spacer(
-            flex: 6,
           )
         ],
       ),
