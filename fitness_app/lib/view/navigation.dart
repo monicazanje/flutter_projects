7d4804909a27220668ea50096eba18e2a3aae83f
monica zanje <fluttermonika@gmail.com>
2024-08-02 21:23:56 +0530

    fitness  app

--- fitness_app/lib/view/navigation.dart
+++ fitness_app/lib/view/navigation.dart
@@ -3,7 +3,7 @@ import 'package:fitness_app/view/exploral_screen.dart';
 import 'package:fitness_app/view/home_screen.dart';
 import 'package:fitness_app/view/workout_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
 
 class BottomNavigator extends StatefulWidget {
   const BottomNavigator({super.key});
@@ -13,18 +13,12 @@ class BottomNavigator extends StatefulWidget {
 
 class _BottomNavigator extends State {
   int _selectindex = 0;
-  bool fillhome = false;
-  bool fillhistory = false;
-  bool fillcard = false;
-  bool fillmore = false;
-  void _selectedIcon(int index) {
+  void _onTap(index) {
     setState(() {
       _selectindex = index;
     });
-
     switch (index) {
       case 0:
-        fillhome = true;
         Navigator.push(
           context,
           MaterialPageRoute(
@@ -33,9 +27,9 @@ class _BottomNavigator extends State {
             },
           ),
         );
+        break;
 
       case 1:
-        fillhistory = true;
         Navigator.push(
           context,
           MaterialPageRoute(
@@ -44,10 +38,9 @@ class _BottomNavigator extends State {
             },
           ),
         );
+        break;
 
       case 2:
-        fillcard = true;
-
         Navigator.push(
           context,
           MaterialPageRoute(
@@ -56,9 +49,9 @@ class _BottomNavigator extends State {
             },
           ),
         );
+        break;
 
       case 3:
-        fillmore = true;
         Navigator.push(
           context,
           MaterialPageRoute(
@@ -71,127 +64,85 @@ class _BottomNavigator extends State {
     }
   }
 
-  void backstate() {
-    setState(() {
-      _selectindex = 0;
-      fillhome = false;
-      fillhistory = false;
-      fillcard = false;
-      fillmore = false;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.only(left: 20, right: 20, bottom: 10, top: 10),
-      height: 62,
-      width: MediaQuery.of(context).size.width * 0.7,
+      height: 65,
+      padding: const EdgeInsets.all(10),
       decoration: const BoxDecoration(
-        color: Colors.black,
-        borderRadius: BorderRadius.all(Radius.circular(40)),
-        boxShadow: [
-          BoxShadow(
-              color: Colors.black, offset: Offset(20, 20), blurRadius: 10),
-          BoxShadow(
-              color: Colors.black, blurRadius: 10, offset: Offset(-20, 20)),
-        ],
-        border: Border(
-          top: BorderSide(
-            width: 1,
-            // color: Color.fromRGBO(225, 227, 237, 1),
+          borderRadius: BorderRadius.all(
+            Radius.circular(30),
           ),
-        ),
-      ),
-      child: Row(
-        children: [
-          fillhome
-              ? navigationData(0, "assets/Home.png", "Home")
-              : navigationImg(0, "assets/Home.png"),
-          const Spacer(),
-          fillhistory
-              ? navigationData(1, "assets/Box 5.png", "History")
-              : navigationImg(1, "assets/Box 5.png"),
-          const Spacer(),
-          fillcard
-              ? navigationData(2, "assets/box4.png", "Card")
-              : navigationImg(2, "assets/box4.png"),
-          const Spacer(),
-          fillmore
-              ? navigationData(3, "assets/Profile.png", "More")
-              : navigationImg(3, "assets/Profile.png"),
-          // navigationImg(0, "assets/Home.png"),
-          // const Spacer(
-          //   flex: 1,
-          // ),
-          // navigationImg(1, "assets/Box 5.png"),
-          // const Spacer(
-          //   flex: 1,
-          // ),
-          // navigationImg(2, "assets/box4.png"),
-          // const Spacer(
-          //   flex: 1,
-          // ),
-          // navigationImg(3, "assets/Profile.png")
-        ],
-      ),
-    );
-  }
-
-  Widget navigationImg(int index, String icon) {
-    // bool isselecte = _selectindex == index;
-    //Size size = MediaQuery.of(context).size;
-
-    return InkWell(
-      onTap: () {
-        _selectedIcon(index);
-        setState(
-          () {
-            _selectindex = index;
-          },
-        );
-      },
-      child: Image(
-        image: AssetImage(icon),
-        height: 30,
-        width: 30,
-        fit: BoxFit.fill,
-      ),
-    );
-  }
-
-  // ignore: non_constant_identifier_names
-  Widget navigationData(int index, String icon, String label) {
-    return Container(
-      height: 36,
-      width: 120,
-      padding: const EdgeInsets.only(left: 10, top: 5, bottom: 5),
-      decoration: const BoxDecoration(
-        borderRadius: BorderRadius.all(
-          Radius.circular(20),
-        ),
-        color: Color.fromRGBO(187, 242, 70, 1),
-      ),
-      child: Row(
-        children: [
-          Image(
-            image: AssetImage(icon),
-            height: 30,
-            width: 30,
-            fit: BoxFit.fill,
-            color: Colors.black,
+          color: Colors.black),
+      child: GNav(
+        haptic: true,
+        curve: Curves.easeOutExpo,
+        duration: const Duration(milliseconds: 900),
+        gap: 8,
+        color: Colors.white,
+        activeColor: Colors.black,
+        iconSize: 24,
+        tabBackgroundColor: const Color.fromRGBO(187, 242, 70, 1),
+        padding: const EdgeInsets.all(5),
+        onTabChange: _onTap,
+        selectedIndex: _selectindex,
+        tabs: const [
+          GButton(
+            icon: Icons.home,
+            text: 'Home',
+            // onPressed: () {
+            //   Navigator.push(
+            //     context,
+            //     MaterialPageRoute(
+            //       builder: (context) {
+            //         return const HomeScreen();
+            //       },
+            //     ),
+            //   );
+            // },
           ),
-          const SizedBox(
-            width: 5,
+          GButton(
+            icon: Icons.rocket_launch_outlined,
+            text: 'Explore',
+            // onPressed: () {
+            //   Navigator.push(
+            //     context,
+            //     MaterialPageRoute(
+            //       builder: (context) {
+            //         return const ExploreScreen();
+            //       },
+            //     ),
+            //   );
+            // },
           ),
-          Text(
-            label,
-            style: GoogleFonts.poppins(
-              fontSize: 15,
-              fontWeight: FontWeight.w400,
-              color: const Color.fromRGBO(31, 32, 41, 1),
-            ),
+          GButton(
+            icon: Icons.analytics_outlined,
+            text: 'Analytics',
+            // onPressed: () {
+            //   Navigator.push(
+            //     context,
+            //     MaterialPageRoute(
+            //       builder: (context) {
+            //         return const ActivityScreen();
+            //       },
+            //     ),
+            //   );
+            // },
           ),
+          GButton(
+            icon: Icons.person_outline_rounded,
+            text: 'Workout',
+            // onPressed: () {
+            //   Navigator.push(
+            //     context,
+            //     MaterialPageRoute(
+            //       builder: (context) {
+            //         return const WorkoutScreen();
+            //       },
+            //     ),
+            //   );
+            // },
+          )
         ],
       ),
     );
--- fitness_app/lib/view/workout_screen.dart
+++ fitness_app/lib/view/workout_screen.dart
@@ -1,13 +1,363 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-class WorkoutScreen extends StatefulWidget{
+class WorkoutScreen extends StatefulWidget {
   const WorkoutScreen({super.key});
   @override
-  State<WorkoutScreen> createState()=>_WorkoutState();
+  State<WorkoutScreen> createState() => _WorkoutState();
 }
-class _WorkoutState extends State<WorkoutScreen>{
+
+class _WorkoutState extends State<WorkoutScreen> {
   @override
-  Widget build(BuildContext context){
-    return Scaffold();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            margin: const EdgeInsets.only(top: 50, right: 20),
+            child: Row(
+              children: [
+                IconButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  icon: const Icon(
+                    Icons.arrow_back_ios_new_outlined,
+                    size: 16,
+                    color: Color.fromRGBO(255, 255, 255, 1),
+                  ),
+                ),
+                const Spacer(
+                  flex: 1,
+                ),
+                Text(
+                  "Workout",
+                  style: GoogleFonts.lato(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w700,
+                    color: const Color.fromARGB(255, 255, 255, 255),
+                  ),
+                ),
+                const Spacer(
+                  flex: 2,
+                ),
+              ],
+            ),
+          ),
+          Container(
+            height: MediaQuery.of(context).size.height * 0.35,
+            margin: const EdgeInsets.only(left: 15, top: 20, right: 10),
+            child: Stack(
+              children: [
+                Positioned(
+                  bottom: 30,
+                  child: ClipRRect(
+                    borderRadius: const BorderRadius.all(
+                      Radius.circular(20),
+                    ),
+                    child: Image.asset(
+                      "assets/image (2).png",
+                      fit: BoxFit.cover,
+                      width: MediaQuery.of(context).size.width * 0.94,
+                    ),
+                  ),
+                ),
+                Positioned(
+                  right: 0,
+                  left: 0,
+                  bottom: 30,
+                  child: Container(
+                    // height: 500.0,
+                    height: MediaQuery.of(context).size.height * 0.3,
+                    width: MediaQuery.of(context).size.width * 0.7,
+                    decoration: BoxDecoration(
+                      color: Colors.black,
+                      gradient: LinearGradient(
+                        begin: Alignment.bottomCenter,
+                        end: Alignment.center,
+                        colors: [
+                          Colors.black,
+                          const Color.fromARGB(255, 50, 50, 50).withOpacity(0),
+                        ],
+                        stops: const [
+                          0.0,
+                          0.8,
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  bottom: 0,
+                  left: 60,
+                  child: Container(
+                    padding: const EdgeInsets.all(16),
+                    decoration: const BoxDecoration(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(20),
+                      ),
+                      color: Color.fromRGBO(25, 33, 38, 0.3),
+                    ),
+                    child: IntrinsicHeight(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        // crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Container(
+                            height: 32,
+                            width: 32,
+                            padding: const EdgeInsets.all(7),
+                            decoration: const BoxDecoration(
+                                color: Color.fromRGBO(187, 242, 70, 1),
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(7))),
+                            child: Image.asset(
+                              "assets/Group 2.png",
+                              fit: BoxFit.fill,
+                            ),
+                          ),
+                          const SizedBox(
+                            width: 10,
+                          ),
+                          Column(
+                            children: [
+                              Text(
+                                "Time",
+                                style: GoogleFonts.lato(
+                                  fontSize: 10,
+                                  fontWeight: FontWeight.w700,
+                                  color:
+                                      const Color.fromARGB(255, 255, 255, 255),
+                                ),
+                              ),
+                              Text(
+                                "20 min",
+                                style: GoogleFonts.lato(
+                                  fontSize: 12,
+                                  fontWeight: FontWeight.w700,
+                                  color: const Color.fromRGBO(187, 242, 70, 1),
+                                ),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(
+                            width: 30,
+                          ),
+                          const VerticalDivider(
+                            color: Colors.white,
+                            thickness: 2,
+                          ),
+                          const SizedBox(
+                            width: 30,
+                          ),
+                          Container(
+                            height: 32,
+                            width: 32,
+                            padding: const EdgeInsets.all(7),
+                            decoration: const BoxDecoration(
+                                color: Color.fromRGBO(187, 242, 70, 1),
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(7))),
+                            child: Image.asset(
+                              "assets/ic_burn.png",
+                              fit: BoxFit.fill,
+                            ),
+                          ),
+                          const SizedBox(
+                            width: 10,
+                          ),
+                          Column(
+                            children: [
+                              Text(
+                                "Burn",
+                                style: GoogleFonts.lato(
+                                  fontSize: 10,
+                                  fontWeight: FontWeight.w700,
+                                  color:
+                                      const Color.fromARGB(255, 255, 255, 255),
+                                ),
+                              ),
+                              Text(
+                                "95 kcal",
+                                style: GoogleFonts.lato(
+                                  fontSize: 12,
+                                  fontWeight: FontWeight.w700,
+                                  color: const Color.fromRGBO(187, 242, 70, 1),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+          Container(
+            margin:
+                const EdgeInsets.only(left: 20, right: 20, bottom: 10, top: 20),
+            child: Text(
+              "Lower Body Training",
+              style: GoogleFonts.lato(
+                fontSize: 24,
+                fontWeight: FontWeight.w800,
+                color: const Color.fromRGBO(255, 255, 255, 1),
+              ),
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.only(left: 20, right: 20, bottom: 20),
+            child: Text(
+              '''The lower abdomen and hips are the most difficult areas of the body to reduce when we are on a diet. Even so, in this area, especially the legs as a whole,   you can reduce weight even if you don't use tools.''',
+              style: GoogleFonts.lato(
+                fontSize: 15,
+                fontWeight: FontWeight.w400,
+                color: const Color.fromRGBO(255, 255, 255, 1),
+              ),
+            ),
+          ),
+          Row(
+            children: [
+              Container(
+                margin: const EdgeInsets.only(
+                    left: 20, right: 20, bottom: 20, top: 10),
+                child: Text(
+                  "Rounds",
+                  style: GoogleFonts.lato(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w600,
+                    color: const Color.fromRGBO(255, 255, 255, 1),
+                  ),
+                ),
+              ),
+              const Spacer(),
+              Container(
+                margin: const EdgeInsets.only(right: 20),
+                child: Text.rich(
+                  TextSpan(
+                    children: [
+                      TextSpan(
+                        text: "1",
+                        style: GoogleFonts.lato(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500,
+                          color: const Color.fromRGBO(255, 255, 255, 1),
+                        ),
+                      ),
+                      TextSpan(
+                        text: "/",
+                        style: GoogleFonts.lato(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w500,
+                          color: const Color.fromRGBO(255, 255, 255, 1),
+                        ),
+                      ),
+                      TextSpan(
+                        text: "8",
+                        style: GoogleFonts.lato(
+                          fontSize: 12,
+                          fontWeight: FontWeight.w500,
+                          color: const Color.fromRGBO(255, 255, 255, 1),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Expanded(
+            child: SizedBox(
+                child: ListView.builder(
+              itemCount: 6,
+              padding: EdgeInsets.zero,
+              itemBuilder: (context, index) {
+                return Container(
+                  margin: const EdgeInsets.only(
+                      left: 20, right: 20, top: 10, bottom: 10),
+                  // padding: const EdgeInsets.all(10),
+                  decoration: const BoxDecoration(
+                      color: Color.fromRGBO(56, 64, 70, 1),
+                      borderRadius: BorderRadius.all(Radius.circular(20))),
+                  child: Row(
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.all(10),
+                        child: ClipRRect(
+                          borderRadius: const BorderRadius.all(
+                            Radius.circular(10),
+                          ),
+                          child: Image.asset("assets/Image (4).png"),
+                        ),
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Jumping Jacks",
+                            style: GoogleFonts.poppins(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w500,
+                              color: const Color.fromRGBO(255, 255, 255, 1),
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          Text(
+                            "00:30",
+                            style: GoogleFonts.poppins(
+                              fontSize: 13,
+                              fontWeight: FontWeight.w400,
+                              color: const Color.fromRGBO(255, 255, 255, 0.5),
+                            ),
+                          ),
+                        ],
+                      ),
+                      const Spacer(),
+                      Container(
+                        height: 40,
+                        width: 40,
+                        margin: const EdgeInsets.only(right: 20),
+                        decoration: const BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Color.fromRGBO(25, 33, 38, 1)),
+                        child: const Icon(
+                          Icons.arrow_right,
+                          color: Color.fromRGBO(187, 242, 70, 1),
+                          size: 35,
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              },
+            )),
+          ),
+        ],
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: Container(
+        margin: const EdgeInsets.only(left: 20, right: 20, bottom: 20),
+        width: double.infinity,
+        child: FloatingActionButton(
+          onPressed: () {},
+          backgroundColor: const Color.fromRGBO(187, 242, 70, 1),
+          shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(30))),
+          child: Text(
+            "Lets Workout",
+            style: GoogleFonts.poppins(
+                fontSize: 16,
+                fontWeight: FontWeight.w600,
+                color: const Color.fromRGBO(0, 0, 0, 1)),
+          ),
+        ),
+      ),
+    );
   }
 }
