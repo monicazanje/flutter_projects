07b2ed3b4201863c3aab9d1084432a381b944192
monica zanje <fluttermonika@gmail.com>
2024-08-15 01:13:58 +0530

    Travel  app

--- travenor_app/lib/view/onboard.dart
+++ travenor_app/lib/view/onboard.dart
@@ -1,5 +1,4 @@
 import 'package:carousel_slider/carousel_slider.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -30,85 +29,158 @@ class _OnboardState extends State<Onboard> {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Container(
-            // height: MediaQuery.of(context).size.height * 0.8,
+            alignment: Alignment.topCenter,
+            // height: MediaQuery.of(context).size.height * 0.6,
             width: MediaQuery.of(context).size.width * 1,
-            decoration:
-                BoxDecoration(border: Border.all(style: BorderStyle.solid)),
-            child: CarouselSlider.builder(
-              itemCount: 3,
-              itemBuilder: (context, index, realIndex) {
-                final imageurl = Cursor.cursorlist[index];
-                return Stack(
-                  children: [
-                    Image.asset(
-                      imageurl.img,
-                      fit: BoxFit.cover,
-                      width: double.infinity,
+            child: AspectRatio(
+              aspectRatio: 11 / 12,
+              child: CarouselSlider.builder(
+                itemCount: 3,
+                itemBuilder: (context, index, realIndex) {
+                  final imageurl = Cursor.cursorlist[index];
+                  return ClipRRect(
+                    borderRadius: const BorderRadius.only(
+                      bottomLeft: Radius.circular(40),
+                      bottomRight: Radius.circular(40),
                     ),
-                    Positioned(
-                      bottom: 10,
+                    child: Container(
+                      height: MediaQuery.of(context).size.height * 0.8,
                       child: Container(
-                        padding: const EdgeInsets.only(left: 20, right: 20),
-                        width: 400,
-                        height: 100,
-                        child: Text.rich(
-                          TextSpan(
-                            children: [
-                              TextSpan(
-                                text: imageurl.name,
-                                style: GoogleFonts.aclonica(
-                                  fontSize: 26,
-                                  fontWeight: FontWeight.w600,
-                                  color: const Color.fromARGB(255, 0, 0, 0),
-                                ),
-                              ),
-                              TextSpan(
-                                text: imageurl.dec,
-                                style: GoogleFonts.aclonica(
-                                  fontSize: 26,
-                                  fontWeight: FontWeight.w600,
-                                  color: const Color.fromRGBO(207, 82, 10, 1),
-                                ),
-                              )
-                            ],
-                          ),
+                        alignment: Alignment.topCenter,
+                        child: Image.asset(
+                          imageurl.img,
+                          fit: BoxFit.cover,
+                          height: MediaQuery.of(context).size.height * 0.6,
+                          width: double.infinity,
                         ),
                       ),
                     ),
-                    const SizedBox(
-                      height: 15,
-                    ),
-                    
-                    // Container(
-                    //   margin: const EdgeInsets.only(left: 20, right: 20),
-                    //   height: 70,
-                    //   child: Text(
-                    //     imageurl.detail,
-                    //     style: GoogleFonts.inter(
-                    //       fontSize: 16,
-                    //       fontWeight: FontWeight.w600,
-                    //       color: const Color.fromRGBO(112, 41, 1, 1),
-                    //     ),
-                    //   ),
-                    // )
-                  ],
-                );
-              },
-              options: CarouselOptions(
-                  autoPlay: true,
-                  aspectRatio: 8 / 12,
-                  viewportFraction: 1,
-                  initialPage: 0,
-                  autoPlayAnimationDuration: const Duration(seconds: 3),
-                  onPageChanged: (index, reason) {
-                    setState(() {
-                      currentindex = index;
-                    });
-                  }),
+                  );
+                  // return Column(
+                  //   children: [
+                  //     Container(
+                  //       alignment: Alignment.topCenter,
+                  //       height: MediaQuery.of(context).size.height * 0.5,
+                  //       child: Image.asset(
+                  //         imageurl.img,
+                  //         fit: BoxFit.cover,
+                  //         width: double.infinity,
+                  //       ),
+                  //     ),
+                  //     const SizedBox(
+                  //       height: 30,
+                  //     ),
+                  //     Container(
+                  //       padding: const EdgeInsets.only(left: 20, right: 20),
+                  //       // width: 370,
+                  //       width: MediaQuery.of(context).size.width * 0.9,
+                  //       height: 100,
+                  //       child: Text.rich(
+                  //         TextSpan(
+                  //           children: [
+                  //             TextSpan(
+                  //               text: imageurl.name,
+                  //               style: GoogleFonts.aclonica(
+                  //                 fontSize: 26,
+                  //                 fontWeight: FontWeight.w600,
+                  //                 color: const Color.fromARGB(255, 0, 0, 0),
+                  //               ),
+                  //             ),
+                  //             TextSpan(
+                  //               text: imageurl.dec,
+                  //               style: GoogleFonts.aclonica(
+                  //                 fontSize: 26,
+                  //                 fontWeight: FontWeight.w600,
+                  //                 color: const Color.fromRGBO(207, 82, 10, 1),
+                  //               ),
+                  //             )
+                  //           ],
+                  //         ),
+                  //       ),
+                  //     ),
+                  //     const SizedBox(
+                  //       height: 15,
+                  //     ),
+                  //     Container(
+                  //       margin: const EdgeInsets.only(left: 20, right: 20),
+                  //       height: 70,
+                  //       child: Text(
+                  //         imageurl.detail,
+                  //         style: GoogleFonts.inter(
+                  //           fontSize: 16,
+                  //           fontWeight: FontWeight.w600,
+                  //           color: const Color.fromRGBO(112, 41, 1, 1),
+                  //         ),
+                  //       ),
+                  //     )
+                  //   ],
+                  // );
+                },
+                options: CarouselOptions(
+                    autoPlay: true,
+                    aspectRatio: 8 / 12,
+                    viewportFraction: 1,
+                    initialPage: 0,
+                    autoPlayAnimationDuration: const Duration(seconds: 3),
+                    onPageChanged: (index, reason) {
+                      setState(() {
+                        currentindex = index;
+                      });
+                    }),
+              ),
             ),
           ),
-          // const SizedBox(
-          //   height: 15,
+          // Container(
+          //   padding: const EdgeInsets.all(20),
+          //   child: CarouselSlider.builder(
+          //     itemCount: 3,
+          //     itemBuilder: (context, index, realIndex) {
+          //       final imageurl = Cursor.cursorlist[index];
+          //       return Column(
+          //         children: [
+          //           Container(
+          //             // width: 370,
+          //             margin: const EdgeInsets.all(20),
+          //             width: MediaQuery.of(context).size.width * 0.9,
+          //             height: 70,
+          //             child: Text.rich(
+          //               TextSpan(
+          //                 children: [
+          //                   TextSpan(
+          //                     text: imageurl.name,
+          //                     style: GoogleFonts.aclonica(
+          //                       fontSize: 26,
+          //                       fontWeight: FontWeight.w600,
+          //                       color: const Color.fromARGB(255, 0, 0, 0),
+          //                     ),
+          //                   ),
+          //                   TextSpan(
+          //                     text: imageurl.dec,
+          //                     style: GoogleFonts.aclonica(
+          //                       fontSize: 26,
+          //                       fontWeight: FontWeight.w600,
+          //                       color: const Color.fromRGBO(207, 82, 10, 1),
+          //                     ),
+          //                   )
+          //                 ],
+          //               ),
+          //             ),
+          //           ),
+          //         ],
+          //       );
+          //     },
+          //     options: CarouselOptions(
+          //         autoPlay: true,
+          //         aspectRatio: 8 / 12,
+          //         viewportFraction: 1,
+          //         initialPage: 0,
+          //         autoPlayAnimationDuration: const Duration(seconds: 3),
+          //         onPageChanged: (index, reason) {
+          //           setState(() {
+          //             currentindex = index;
+          //           });
+          //         }),
+          //   ),
           // ),
           const Spacer(),
           Row(
@@ -140,6 +212,27 @@ class _OnboardState extends State<Onboard> {
                       ),
             ],
           ),
+          Container(
+            margin: const EdgeInsets.all(30),
+            padding: const EdgeInsets.all(10),
+            width: MediaQuery.of(context).size.width * 0.8,
+            height: 50,
+            decoration: const BoxDecoration(
+                color: Color.fromRGBO(13, 110, 253, 1),
+                borderRadius: BorderRadius.all(Radius.circular(20))),
+            child: GestureDetector(
+              onTap: () {},
+              child: Text(
+                "Get Started",
+                textAlign: TextAlign.center,
+                style: GoogleFonts.poppins(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w600,
+                  color: const Color.fromRGBO(255, 255, 255, 1),
+                ),
+              ),
+            ),
+          )
         ],
       ),
     );
