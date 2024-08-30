f5dfa12079a106b99984ea7e74786e0c9d46c405
monica zanje <fluttermonika@gmail.com>
2024-08-30 13:39:19 +0530

    fashion  app

--- music_app/lib/main.dart
+++ music_app/lib/main.dart
@@ -1,7 +1,5 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:music_app/controller/productcontroller.dart';
 import 'package:music_app/view/producthome.dart';
 
 void main() {
@@ -20,16 +18,9 @@ class MainApp extends StatelessWidget {
   const MainApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        Provider(create: (context) {
-          return ProductController();
-        }),
-      ],
-      child: const MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home:HomePage(),
-      ),
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home:HomePage(),
     );
   }
 }
